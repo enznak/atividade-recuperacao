a87cf42cc386ed913b10baaa5f1e9d8319247db8
Enzo Nakanishi <enzo.mazakina@sptech.school>
2026-07-15 17:48:45 -0300

    Add files via upload

---
+++ src/database/script-tabelas.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE livrariaRecuperacao20262;
+
+USE livrariaRecuperacao20262;
+select * from livro;
+
+CREATE TABLE autor (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50)
+);
+
+CREATE TABLE genero (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50)
+);
+
+insert into genero values
+(default, 'horror'),
+(default, 'romance'),
+(default, 'poesia'),
+(default, 'fantasia');
+
+CREATE TABLE livro (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(50),
+    fkAutor INT,
+    fkGenero INT,
+    precoCompra DOUBLE,
+    precoVenda DOUBLE,
+    CONSTRAINT fk_livro_autor FOREIGN KEY (fkAutor) REFERENCES autor(id),
+    CONSTRAINT fk_livro_genero FOREIGN KEY (fkGenero) REFERENCES genero(id)
+);
